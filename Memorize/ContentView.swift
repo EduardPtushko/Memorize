13fe0da7cc29de012793a4b07b31c1d1ccdb7e8a
Эдуард Птушко <ptushko@gmail.com>
2024-03-29 12:33:12 +0300

    Toggble isFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,16 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: false)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundStyle(.orange)
         .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct CardView: View {
+    @State var isFaceUp = true
+
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                base.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
 }
